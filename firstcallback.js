e4d8a11c486bedce648139560f4ac458920ec9b4
Adib <adibalamir@hotmail.com>
2018-06-07 15:37:54 +0000

    first callback ex

---
+++ firstcallback.js
@@ -0,0 +1,11 @@
+function findWaldo(arr, found) {
+  arr.forEach(function(element) {
+    if (element === "Waldo") {
+      found(arr.indexOf(element));   // execute callback
+    }
+  })
+}
+function actionWhenFound(arg) {
+  console.log("Found Waldo at index " + arg + "!");
+}
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
